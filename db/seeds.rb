b5ce12b4b318767ce589ca4cca5ff2bf83674be0
Maud Payan <maudpayan@gmail.com>
2019-02-27 15:02:59 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Restaurant.destroy_all
+Review.destroy_all
+
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.cell_phone_with_country_code
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  new_restaurant = Restaurant.create(name: name, address: address, phone_number: phone_number, category: category)
+  6.times do
+    content = Faker::Restaurant.review
+    rating = rand(0..5)
+    new_review = Review.create(content: content, rating: rating, restaurant: new_restaurant)
+  end
+end
+
